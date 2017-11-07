fb0826013c750f57c678dd27f002ba1cdb1b09b4
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-11-07 21:03:08 +0100

    Update references

--- src/bio2bel_chebi/cli.py
+++ src/bio2bel_chebi/cli.py
@@ -25,7 +25,7 @@ def main():
 @click.option('-o', '--output', type=click.File('w'), default=sys.stdout)
 def write(ols_base, output):
     """Writes BEL namespace"""
-    ontology = OlsNamespaceOntology(MODULE_NAME, MODULE_DOMAIN, MODULE_FUNCTIONS, ols_base=ols_base)
+    ontology = OlsNamespaceOntology(MODULE_NAME, MODULE_DOMAIN, bel_function=MODULE_FUNCTIONS, ols_base=ols_base)
     ontology.write(output)
 
 
@@ -34,7 +34,7 @@ def write(ols_base, output):
 @click.option('--no-hash-check', is_flag=True)
 def deploy(ols_base=None, no_hash_check=False):
     """Deploy to Artifactory"""
-    ontology = OlsNamespaceOntology(MODULE_NAME, MODULE_DOMAIN, MODULE_FUNCTIONS, ols_base=ols_base)
+    ontology = OlsNamespaceOntology(MODULE_NAME, MODULE_DOMAIN, bel_function=MODULE_FUNCTIONS, ols_base=ols_base)
     success = ontology.deploy(hash_check=(not no_hash_check))
     click.echo('Deployed to {}'.format(success) if success else 'Duplicate not deployed')
 
--- src/bio2bel_chebi/run.py
+++ src/bio2bel_chebi/run.py
@@ -6,16 +6,16 @@ from pybel_tools.ols_utils import OlsNamespaceOntology
 __all__ = [
     'MODULE_NAME',
     'MODULE_DOMAIN',
-    'MODULE_FUNCTIONS',
+    'MODULE_FUNCTION',
     'write_belns',
     'deploy_to_arty',
 ]
 
 MODULE_NAME = 'chebi'
 MODULE_DOMAIN = NAMESPACE_DOMAIN_CHEMICAL
-MODULE_FUNCTIONS = 'A'
+MODULE_FUNCTION = 'A'
 
-ontology = OlsNamespaceOntology(MODULE_NAME, MODULE_DOMAIN, MODULE_FUNCTIONS)
+ontology = OlsNamespaceOntology(MODULE_NAME, MODULE_DOMAIN, bel_function=MODULE_FUNCTION)
 
-write_belns = ontology.write
-deploy_to_arty = ontology.deploy
+write_belns = ontology.write_namespace
+deploy_to_arty = ontology.deploy_namespace
