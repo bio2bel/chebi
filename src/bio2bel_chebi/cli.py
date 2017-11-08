2a6ef3432f6841b30f333a0fc38a12812bd8fa4b
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-11-08 13:51:59 +0100

    Update references

--- src/bio2bel_chebi/cli.py
+++ src/bio2bel_chebi/cli.py
@@ -25,8 +25,8 @@ def main():
 @click.option('-o', '--output', type=click.File('w'), default=sys.stdout)
 def write(ols_base, output):
     """Writes BEL namespace"""
-    ontology = OlsNamespaceOntology(MODULE_NAME, MODULE_DOMAIN, MODULE_FUNCTIONS, ols_base=ols_base)
-    ontology.write(output)
+    ontology = OlsNamespaceOntology(MODULE_NAME, MODULE_DOMAIN, bel_function=MODULE_FUNCTIONS, ols_base=ols_base)
+    ontology.write_namespace(output)
 
 
 @main.command()
@@ -34,7 +34,7 @@ def write(ols_base, output):
 @click.option('--no-hash-check', is_flag=True)
 def deploy(ols_base=None, no_hash_check=False):
     """Deploy to Artifactory"""
-    ontology = OlsNamespaceOntology(MODULE_NAME, MODULE_DOMAIN, MODULE_FUNCTIONS, ols_base=ols_base)
+    ontology = OlsNamespaceOntology(MODULE_NAME, MODULE_DOMAIN, bel_function=MODULE_FUNCTIONS, ols_base=ols_base)
     success = ontology.deploy(hash_check=(not no_hash_check))
     click.echo('Deployed to {}'.format(success) if success else 'Duplicate not deployed')
 
