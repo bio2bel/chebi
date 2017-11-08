a3a82b338478d648b7a769ae748809cfecfa40f0
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-11-08 13:52:15 +0100

    smarter loading of defaults

--- src/bio2bel_chebi/constants.py
+++ src/bio2bel_chebi/constants.py
@@ -8,9 +8,10 @@ INCHIS_URL = 'ftp://ftp.ebi.ac.uk/pub/databases/chebi/Flat_file_tab_delimited/ch
 from os import environ, makedirs, path
 
 #: The default directory where PyBEL files, including logs and the  default cache, are stored. Created if not exists.
-DATA_DIR = environ.get('CHEBI_RESOURCE_DIRECTORY', path.join(path.expanduser('~'), '.pybel', 'bio2bel', 'chebi'))
+BIO2BEL_DIR = environ.get('BIO2BEL_DIRECTORY', path.join(path.expanduser('~'), '.pybel', 'bio2bel'))
+DATA_DIR = path.join(BIO2BEL_DIR, 'chebi')
 makedirs(DATA_DIR, exist_ok=True)
 
 DEFAULT_CACHE_NAME = 'chebi.db'
 DEFAULT_CACHE_LOCATION = path.join(DATA_DIR, DEFAULT_CACHE_NAME)
-DEFAULT_CACHE_CONNECTION = 'sqlite:///' + DEFAULT_CACHE_LOCATION
+DEFAULT_CACHE_CONNECTION = environ.get('BIO2BEL_CHEBI_DB', 'sqlite:///' + DEFAULT_CACHE_LOCATION)
