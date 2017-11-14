8bf7e99fae1f982989e15aaad72fc62148832bcf
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-11-14 17:23:10 +0100

    Update deploy

--- src/bio2bel_chebi/cli.py
+++ src/bio2bel_chebi/cli.py
@@ -34,9 +34,9 @@ def write(ols_base, output):
 @click.option('-b', '--ols-base', help="Custom OLS base url")
 @click.option('--no-hash-check', is_flag=True)
 def deploy(ols_base=None, no_hash_check=False):
-    """Deploy to Artifactory"""
+    """Deploy the ChEBI namespace to Artifactory"""
     ontology = OlsNamespaceOntology(MODULE_NAME, MODULE_DOMAIN, encoding=MODULE_ENCODING, ols_base=ols_base)
-    success = ontology.deploy(hash_check=(not no_hash_check))
+    success = ontology.deploy_namespace(hash_check=(not no_hash_check))
     click.echo('Deployed to {}'.format(success) if success else 'Duplicate not deployed')
 
 
