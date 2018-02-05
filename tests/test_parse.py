3069cd79ce0a76586b1b652ec9609a9cdfb58ee6
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-02-05 13:07:24 +0100

    Update tox

---
+++ tests/test_parse.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+
+class TestParse(unittest.TestCase):
+    def test_dummy(self):
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
